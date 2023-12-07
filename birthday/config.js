5132c0c6c281450bdc405acee85fdfe431b7b390
ducanh273 <ducanhtq88@gmail.com>
2023-12-07 22:41:25 +0700

    change date

--- birthday/config.js
+++ birthday/config.js
@@ -1,15 +1,15 @@
 
 var config = {
     texts: [
-        "Chúc mừng sinh nhật",  
-        "Trần Thị Luyên",  
+        "Chúc mừng sinh nhật Em bé của Anh",
+        "trabitzz",   
         "Hãy biến ngày hôm nay trở thành một ngày đặc biệt đầy niềm vui, hạnh phúc và thành công trong cuộc sống.",
         "Mong rằng tuổi mới này sẽ đem đến nhiều niềm vui, sức khỏe tốt và đủ sự thành công để làm tỏa sáng cuộc sống của mình.",
         "Hãy trân trọng những khả năng và thành tựu của mình, và luôn luôn tin tưởng vào bản thân để đạt được những mục tiêu lớn trong cuộc sống.",
-        "Và hy vọng bạn sẽ có một ngày đầy niềm vui, tiếng cười và những kỷ niệm đáng nhớ cùng những người thân yêu của mình.",
+        "Và hy vọng Em sẽ có một ngày đầy niềm vui, tiếng cười và những kỷ niệm đáng nhớ cùng những người thân yêu của mình.",
     ],
     imgs: {
-        "Trần Thị Luyên": "./imgs/cmsn.png",
+        "trabitzz": "./imgs/cmsn.png",
         
     },
     desc: {
@@ -20,7 +20,7 @@ var config = {
         cake_fadein: "Bánh sinh nhật",
         light_candle: "Hãy thắp nến nào.",
         wish_message: "Hãy ước đi nào.",
-        story: "Vài lời chúc dành cho bạn.",
+        story: "Vài lời chúc dành cho Em.",
         cake_cut: "Còn 1 điều bất ngờ",
     }
 };
